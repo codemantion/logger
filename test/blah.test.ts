9f4da2320be790dcc22a3adccb6c6aa86f882927
Santanu Prasad Sahoo <sahoosantanu92@gmail.com>
2022-01-08 14:00:40 +0530

    Test issue fixes

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,7 +1,17 @@
-import { sum } from '../src';
+import logger from '../src';
 
 describe('blah', () => {
   it('works', () => {
-    expect(sum(1, 1)).toEqual(2);
+    logger.log('log', 'log message');
+    logger.info('info message');
+    logger.warn('warn message');
+    logger.error('error message');
+    logger.dir({
+      name: 'santanu',
+      nested: {
+        key1: 'value1',
+        key2: 'value2',
+      },
+    });
   });
 });
