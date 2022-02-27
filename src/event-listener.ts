f5f3d6b76ce7759aff49399bd0c6cb531814f979
Santanu Prasad Sahoo <sahoosantanu92@gmail.com>
2022-02-27 17:36:34 +0530

    Eslint upgrade and lint fixes

--- src/event-listener.ts
+++ src/event-listener.ts
@@ -1,35 +1,39 @@
-import {Type} from "./index";
+import { Type } from './index';
 
 class LoggerEventListener extends EventTarget {
-  on(type: Type | '*', listener: EventListener): void {
-    if (type !== '*' && type !== 'log'&& type !== 'error' && type !== 'warn' && type !== 'info' && type !== 'dir')
-      throw new Error('Invalid event type');
-    this.addEventListener(type, listener);
-  }
+	on(type: Type | '*', listener: EventListener): void {
+		if (type !== '*' && type !== 'log' && type !== 'error' && type !== 'warn' && type !== 'info' && type !== 'dir')
+			throw new Error('Invalid event type');
+		this.addEventListener(type, listener);
+	}
 
-  off(type: Type | '*', listener: EventListener): void {
-    if (type !== '*' && type !== 'log'&& type !== 'error' && type !== 'warn' && type !== 'info' && type !== 'dir')
-      throw new Error('Invalid event type');
-    this.removeEventListener(type, listener);
-  }
+	off(type: Type | '*', listener: EventListener): void {
+		if (type !== '*' && type !== 'log' && type !== 'error' && type !== 'warn' && type !== 'info' && type !== 'dir')
+			throw new Error('Invalid event type');
+		this.removeEventListener(type, listener);
+	}
 
-  dispatch(type: Type, messages: any[], originalMessages: any[]): void {
-    const detail = {
-      type,
-      messages,
-      originalMessages
-    };
-    this.dispatchEvent(new CustomEvent(type, {
-      detail,
-      bubbles: true,
-      cancelable: true
-    }));
-    this.dispatchEvent(new CustomEvent('*', {
-      detail,
-      bubbles: true,
-      cancelable: true
-    }));
-  }
+	dispatch(type: Type, messages: any[], originalMessages: any[]): void {
+		const detail = {
+			type,
+			messages,
+			originalMessages,
+		};
+		this.dispatchEvent(
+			new CustomEvent(type, {
+				detail,
+				bubbles: true,
+				cancelable: true,
+			}),
+		);
+		this.dispatchEvent(
+			new CustomEvent('*', {
+				detail,
+				bubbles: true,
+				cancelable: true,
+			}),
+		);
+	}
 }
 
 export default LoggerEventListener;
--- src/index.ts
+++ src/index.ts
@@ -1,171 +1,160 @@
-import LoggerEventListener from "./event-listener";
+import LoggerEventListener from './event-listener';
 
 export declare type Type = 'log' | 'info' | 'warn' | 'error' | 'dir';
 
 export declare type LoggerConfig = {
-  isEnable: boolean;
-  name: string;
-  styles: {
-    label: {
-      log: string;
-      info: string;
-      warn: string;
-      error: string;
-      dir: string;
-    };
-  };
-  templates: {
-    log: string;
-    timestamp: string;
-  };
-  isShowLabel: boolean;
-  isShowTimestamp: boolean;
-  isUseNative: boolean;
-  isPrintOnConsole: boolean;
+	isEnable: boolean;
+	name: string;
+	styles: {
+		label: {
+			log: string;
+			info: string;
+			warn: string;
+			error: string;
+			dir: string;
+		};
+	};
+	templates: {
+		log: string;
+		timestamp: string;
+	};
+	isShowLabel: boolean;
+	isShowTimestamp: boolean;
+	isUseNative: boolean;
+	isPrintOnConsole: boolean;
 };
 
 type ObjectType = { [key: string]: any };
 
-function parseArgs<T = string>(
-  args: T[],
-  defaultType: Type = 'log'
-): { type: Type; messages: T[] } {
-  if (args.length === 1) {
-    return { type: defaultType, messages: args };
-  }
-  const [type, ...messages] = args;
-  // @ts-ignore
-  if (type === 'log' || type === 'info' || type === 'warn' || type === 'error')
-    // @ts-ignore
-    return { type, messages };
-  else
-    return {
-      type: defaultType,
-      messages: args,
-    };
+function parseArgs<T = string>(args: T[], defaultType: Type = 'log'): { type: Type; messages: T[] } {
+	if (args.length === 1) {
+		return { type: defaultType, messages: args };
+	}
+	const [type, ...messages] = args;
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	if (type === 'log' || type === 'info' || type === 'warn' || type === 'error')
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		return { type, messages };
+	else
+		return {
+			type: defaultType,
+			messages: args,
+		};
 }
 
 function getMessages(...args: (string | string[])[]): [Type, string[], string[]] {
-  // @ts-ignore
-  const _this = this as Logger;
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	const _this = this as Logger;
 
-  const { type, messages } = parseArgs<string>(args as string[]);
+	const { type, messages } = parseArgs<string>(args as string[]);
 
-  let timestampTemplate = _this.config.templates.timestamp;
+	let timestampTemplate = _this.config.templates.timestamp;
 
-  if (_this.config.isShowTimestamp)
-    timestampTemplate = timestampTemplate.replace(
-      '%timestamp%',
-      String(new Date().toISOString())
-    );
-  else timestampTemplate = timestampTemplate.replace('%timestamp%', '');
+	if (_this.config.isShowTimestamp)
+		timestampTemplate = timestampTemplate.replace('%timestamp%', String(new Date().toISOString()));
+	else timestampTemplate = timestampTemplate.replace('%timestamp%', '');
 
-  if (_this.config.isShowLabel) {
-    let template = _this.config.templates.log;
-    template = template.replace('%name%', _this.config.name);
-    template = template.replace('%type%', type);
-    return [
-      type,
-      [
-        `%c${template}`,
-        _this.config.styles.label[type],
-        timestampTemplate,
-        ...messages,
-      ],
-      messages
-    ];
-  } else {
-    return [type, [timestampTemplate, ...messages], messages];
-  }
+	if (_this.config.isShowLabel) {
+		let template = _this.config.templates.log;
+		template = template.replace('%name%', _this.config.name);
+		template = template.replace('%type%', type);
+		return [type, [`%c${template}`, _this.config.styles.label[type], timestampTemplate, ...messages], messages];
+	} else {
+		return [type, [timestampTemplate, ...messages], messages];
+	}
 }
 
 interface LoggerType {
-  config: LoggerConfig;
-  log(...message: string[]): void;
-  log(type: Type, ...message: string[]): void;
-  info(...message: string[]): void;
-  warn(...message: string[]): void;
-  error(...message: string[]): void;
-  dir(...data: ObjectType[]): void;
-  dir(type: Type, ...data: ObjectType[]): void;
+	config: LoggerConfig;
+	log(...message: string[]): void;
+	log(type: Type, ...message: string[]): void;
+	info(...message: string[]): void;
+	warn(...message: string[]): void;
+	error(...message: string[]): void;
+	dir(...data: ObjectType[]): void;
+	dir(type: Type, ...data: ObjectType[]): void;
 }
 
 export class Logger extends LoggerEventListener implements LoggerType {
-  config: LoggerConfig = {
-    isEnable: true,
-    name: 'logger',
-    styles: {
-      label: {
-        log: 'background: #222; color: #bada55',
-        info: 'background: #222; color: #bada55',
-        warn: 'background: #ffdd76; color: #222',
-        error: 'background: #ffc0c0; color: #ff0000',
-        dir: 'background: #222; color: #bada55',
-      },
-    },
-    templates: {
-      log: '[%name%]:%type%',
-      timestamp: '%timestamp%:',
-    },
-    isShowLabel: true,
-    isShowTimestamp: true,
-    isUseNative: false,
-    isPrintOnConsole: true,
-  };
+	config: LoggerConfig = {
+		isEnable: true,
+		name: 'logger',
+		styles: {
+			label: {
+				log: 'background: #222; color: #bada55',
+				info: 'background: #222; color: #bada55',
+				warn: 'background: #ffdd76; color: #222',
+				error: 'background: #ffc0c0; color: #ff0000',
+				dir: 'background: #222; color: #bada55',
+			},
+		},
+		templates: {
+			log: '[%name%]:%type%',
+			timestamp: '%timestamp%:',
+		},
+		isShowLabel: true,
+		isShowTimestamp: true,
+		isUseNative: false,
+		isPrintOnConsole: true,
+	};
 
-  constructor(Config?: Partial<LoggerConfig>) {
-    super();
-    this.config = {
-      ...this.config,
-      ...Config,
-    };
-  }
+	constructor(Config?: Partial<LoggerConfig>) {
+		super();
+		this.config = {
+			...this.config,
+			...Config,
+		};
+	}
 
-  log(...args: (string | string[])[]): void {
-    if (!this.config.isEnable) return;
-    const [type, messages, originalMessage] = getMessages.call(this, ...args);
-    if (this.config.isPrintOnConsole) {
-      if (this.config.isUseNative) {
-        if (type === 'log' || type === 'info') console.log(...messages);
-        if (type === 'warn') console.warn(...messages);
-        if (type === 'error') console.error(...messages);
-      } else console.log(...messages);
-    }
-    this.dispatch(type, messages, originalMessage);
-  }
+	log(...args: (string | string[])[]): void {
+		if (!this.config.isEnable) return;
+		const [type, messages, originalMessage] = getMessages.call(this, ...args);
+		if (this.config.isPrintOnConsole) {
+			if (this.config.isUseNative) {
+				// eslint-disable-next-line no-console
+				if (type === 'log' || type === 'info') console.log(...messages);
+				// eslint-disable-next-line no-console
+				if (type === 'warn') console.warn(...messages);
+				// eslint-disable-next-line no-console
+				if (type === 'error') console.error(...messages);
+				// eslint-disable-next-line no-console
+			} else console.log(...messages);
+		}
+		this.dispatch(type, messages, originalMessage);
+	}
 
-  info(...message: string[]): void {
-    this.log('info', ...message);
-  }
+	info(...message: string[]): void {
+		this.log('info', ...message);
+	}
 
-  warn(...message: string[]): void {
-    this.log('warn', ...message);
-  }
+	warn(...message: string[]): void {
+		this.log('warn', ...message);
+	}
 
-  error(...message: string[]): void {
-    this.log('error', ...message);
-  }
+	error(...message: string[]): void {
+		this.log('error', ...message);
+	}
 
-  dir(...args: (string | { [key: string]: any })[]): void {
-    const { type, messages } = parseArgs<string | { [key: string]: any }>(
-      args,
-      'info'
-    );
+	dir(...args: (string | { [key: string]: any })[]): void {
+		const { type, messages } = parseArgs<string | { [key: string]: any }>(args, 'info');
 
-    const result = messages.map(each => JSON.stringify(each, null, 2));
-    this.log(type, ...result);
-  }
+		const result = messages.map((each) => JSON.stringify(each, null, 2));
+		this.log(type, ...result);
+	}
 
-  setConfig(config: Partial<LoggerConfig>): void {
-    this.config = {
-      ...this.config,
-      ...config,
-    };
-  }
+	setConfig(config: Partial<LoggerConfig>): void {
+		this.config = {
+			...this.config,
+			...config,
+		};
+	}
 
-  setEnable(isEnable: boolean): void {
-    this.config.isEnable = isEnable;
-  }
+	setEnable(isEnable: boolean): void {
+		this.config.isEnable = isEnable;
+	}
 }
 
 const logger = new Logger();
