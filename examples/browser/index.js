ee83dc46b5a59b065132f03522ec2ca19274751e
Santanu Prasad Sahoo <sahoosantanu92@gmail.com>
2022-07-06 00:42:53 +0530

    Log label and clone added (#60)
    * Event listeners: now we can add event listenrs to the logger (#2)
    
    * Event listeners: now we can add event listeners to the logger
    
    * Eslint upgrade and lint fixes
    
    * v0.1.3-pre.3
    
    * v0.1.3-pre.4
    
    * v0.1.3-pre.5
    
    * Log label and clone added
    
    * Test issue fixes

--- examples/browser/index.js
+++ examples/browser/index.js
@@ -1,23 +1,64 @@
-import logger from "../../dist";
+import logger from '../../dist';
 
 // logger.setConfig({ isUseNative: true });
 
 const onLog = (log) => {
-  console.log(log);
+	console.log(log);
 };
 
-logger.on("*", onLog);
+logger.on('*', onLog);
 
-logger.log("log", "log message");
-logger.info("info message");
-logger.warn("warn message");
-logger.error("error message");
-logger.dir({
-    name: "santanu",
-    nested: {
-        key1: "value1",
-        key2: "value2",
-    }
-});
+logger.log('log', 'log message');
+// logger.new().log('log', 'log message jjj');
+// logger.info('info message');
+// logger.warn('warn message');
+// logger.error('error message');
+// logger.dir({
+// 	name: 'santanu',
+// 	nested: {
+// 		key1: 'value1',
+// 		key2: 'value2',
+// 	},
+// });
 
-logger.off('*', onLog);
+function _getCallerFile() {
+	var filename;
+	let lineNumber;
+
+	var _pst = Error.prepareStackTrace;
+	Error.prepareStackTrace = function (err, stack) {
+		return stack;
+	};
+	try {
+		var err = new Error();
+		var callerfile;
+		var currentfile;
+
+		currentfile = err.stack.shift().getFileName();
+
+		while (err.stack.length) {
+			callerfile = err.stack.shift().getFileName();
+			lineNumber = err.stack.shift().getLineNumber();
+
+			if (currentfile !== callerfile) {
+				filename = callerfile;
+				break;
+			}
+		}
+	} catch (err) {}
+	Error.prepareStackTrace = _pst;
+
+	return filename + ':' + lineNumber;
+}
+
+// console.log(_getCallerFile());
+
+logger.with('ddd').log('Test');
+
+logger.label('Test').log('DDDDDDDDDDDDDD');
+
+logger.dir({ name: 'sa', d: { f: '' } });
+
+// logger.log = null;
+// console.log(logger.print);
+// logger.off('*', onLog);
